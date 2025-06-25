829df03e181c8bf3d1efd46a4e712a01262a60f3
apipen-young <jungyunsoo0713@gmail.com>
2025-06-26 08:52:52 +0900

    Initial commit

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,33 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+
+// export async function GET(context) {
+// 	const posts = await getCollection('blog');
+// 	return rss({
+// 		title: SITE_TITLE,
+// 		description: SITE_DESCRIPTION,
+// 		site: context.site,
+// 		items: posts.map((post) => ({
+// 			...post.data,
+// 			link: `/blog/${post.id}/`,
+// 		})),
+// 	});
+// }
+//
+export async function GET(context) {
+	const blogPosts = await getCollection('blog');
+	const oauthPosts = await getCollection('oauth'); // 너가 새로 추가한 컬렉션 이름
+
+	const allPosts = [...blogPosts, ...oauthPosts];
+
+	return rss({
+		title: SITE_TITLE,
+		description: SITE_DESCRIPTION,
+		site: context.site,
+		items: allPosts.map((post) => ({
+			...post.data,
+			link: `/${post.collection}/${post.slug}/`, // collection별 경로 처리
+		})),
+	});
+}
